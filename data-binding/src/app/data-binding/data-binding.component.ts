a8c748de83d5be0771d904c4f180d7f4f1b57466
Heitor A Neto <heitorh3@gmail.com>
2017-06-24 17:25:28 -0300

    Event bind

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,17 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+    .highlight{
+        background-color: yellowgreen;
+        font-weight: bold;
+    }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://loaine.traing';
   urlImagen: string = 'http://lorempixel.com/400/200/sports/';
+
+  valorAtual: string = '';
+  valorSalvo = '';
   
+  isonMouseOver: boolean = false;
+
   cursoAngular: boolean = true;
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert("Botão clicado");
+  }
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isonMouseOver = ! this.isonMouseOver;
   }
 
   getValor(){
