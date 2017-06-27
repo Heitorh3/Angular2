5957118c192ec690cc8327d21f66e1a91e04e193
Heitor <heitorh3@gmail.com>
2017-06-27 14:33:14 -0300

    Diretiva: ngFor

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgSwitchCaseComponent } from './diretiva-ng-switch-case/diretiva-ng-switch-case.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    DiretivaNgSwitchCaseComponent
+    DiretivaNgSwitchCaseComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule
