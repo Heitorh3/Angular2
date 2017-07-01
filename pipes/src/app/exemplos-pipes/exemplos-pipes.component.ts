da0acaceac1a79d227d2b3819bec42a9018ddb81
Heitor A Neto <heitorh3@gmail.com>
2017-07-01 15:42:48 -0300

    Pipes: Async (assíncrono)

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Rx";
+
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -35,6 +37,14 @@ export class ExemplosPipesComponent implements OnInit {
     );
 
   }
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
+
+  valorAsync2 = Observable.interval(2000)
+    .map(valor => 'Valor assíncrono 2');
+
 
   addCurso(valor){
     this.livros.push(valor);
