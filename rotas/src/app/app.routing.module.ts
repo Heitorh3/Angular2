60beac0e04d75e0672fe86860c4007fabea92bb6
Heitor A Neto <heitorh3@gmail.com>
2017-07-03 20:53:40 -0300

    Rotas: Criando um módulo de rotas de funcionalidade

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 
 const appRoutes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'cursos', component: CursosComponent},
-    {path: 'cursos/:id', component: CursoDetalheComponent},
-    {path: 'naoEcontrado', component: CursoNaoEncontradoComponent}
 ];
 
 @NgModule({
--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos.component';
 import { CursosService } from './cursos.service';
+import { CursosRoutingModule } from './cursos.routing.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        CursosRoutingModule
     ],
     exports: [],
     declarations: [
