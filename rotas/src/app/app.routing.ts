2e0a1435930bf0eebdcbcae54497128803e2ae03
Heitor <heitorh3@gmail.com>
2017-07-03 16:22:09 -0300

    Criando um módulo de funcionalidade

--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -1,11 +1,11 @@
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 
 const APP_ROUTES: Routes = [
 
