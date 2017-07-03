0a945a9f91db6ef228bfebb352fa2c42fdcef514
Heitor <heitorh3@gmail.com>
2017-07-03 15:34:27 -0300

    Definindo e extraindo parâmetros de url (query)

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,7 +21,6 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     routing
   ],
   providers: [CursosService],
