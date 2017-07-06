a60c85b88dd2d6c809c748bc2d3f5532a539789f
Heitor <heitorh3@gmail.com>
2017-07-06 13:15:05 -0300

    Forms (template driven) Inicializando valores com ngModel (two-way data-binding)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Heitor Neto',
+    email: 'heitor@ufu.br'
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
   onSubmit(form){
-    console.log(form);
+    console.log(form.value);
+
+    console.log(this.usuario);
   }
 
 }
