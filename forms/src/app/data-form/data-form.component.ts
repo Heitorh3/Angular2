8b3851631bbf10ce2de309c4dff148858b88c554
Heitor <heitorh3@gmail.com>
2017-07-10 15:53:28 -0300

    #92: Formulários reativos: Fazendo submit

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-data-form',
@@ -10,7 +11,9 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(
+    private _formBuilder: FormBuilder, 
+    private _http: Http) { }
 
   ngOnInit() {
 
@@ -24,6 +27,14 @@ export class DataFormComponent implements OnInit {
       nome: new FormControl('valor inicial'),
       email: new FormControl(null)
     });*/
+  }
+
+  onSubmit(){
+    console.log(this.formulario.value);
+
+     this._http.post('https://httpbin.org/post',JSON.stringify(this.formulario.value))
+              .map(resposta => resposta)
+              .subscribe(dados => console.log(dados));
   }
 
 }
--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -1,10 +1,12 @@
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './../shared/shared.module';
 import { DataFormComponent } from './data-form.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    HttpModule
   ],
   declarations: [
     DataFormComponent
