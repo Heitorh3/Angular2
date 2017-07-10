9ed0a9bd90b04e640a10eaea5f8e67598fab159c
Heitor <heitorh3@gmail.com>
2017-07-10 14:12:33 -0300

    #89: Formulários reativos: Configuração (Módulo e Componente)

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +16,7 @@ import { TemplateFormModule } from './template-form/template-form.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     TemplateFormModule
   ],
   providers: [],
